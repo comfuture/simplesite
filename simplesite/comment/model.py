384759f1b312f694560f78c846a081bb1b37d498
comfuture <comfuture@gmail.com>
2011-02-08 02:07:27 +0900

    comment.model is merged into simplesite.model temporary

--- simplesite/comment/model.py
+++
@@ -1,8 +0,0 @@
-from ..model import *
-
-class Comment(ListContent):
-    using_options(inheritance='multi')
-
-    content = ManyToOne('Content')
-
-Content.comments = OneToMany('Comment', cascade='all')
